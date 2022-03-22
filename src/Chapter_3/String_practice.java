f92f389c4c376208096ba3e344ebd28c63b59f7f
JBEDU20 <JBEDU20@JBEDU-20>
2022-03-22 12:17:00 +0900

    go

---
+++ src/Chapter_3/String_practice.java
@@ -0,0 +1,27 @@
+package Chapter_3;
+
+import java.util.*;
+
+public class String_practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("exit을 입력하면 종료합니다.");
+		
+		while (true) {
+			System.out.print(">>");
+			String text = scanner.nextLine();
+//			String text = scanner.next();
+			System.out.println(text);
+			if (text.equals("exit")) {
+				break;
+			}
+		}
+		System.out.println("프로그램을 종료합니다...");
+
+		scanner.close();
+	}
+
+}
